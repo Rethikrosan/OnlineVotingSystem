936cefadf31371b692c53f5c006d735ab1f44b0a
Your Name <rethik124@gmail.com>
2026-07-06 20:20:24 +0530

    Configured PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     )
 }
 
+DATABASES["default"]["CONN_HEALTH_CHECKS"] = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
